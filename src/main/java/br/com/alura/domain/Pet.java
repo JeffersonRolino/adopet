5fb0195937dfcde0c618990eafb7fa1fb3ec5782
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-14 10:36:24 -0300

    Curso 01 - Capítulo 03 - Aula 03: Usando Pet em PetService

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,4 +1,46 @@
 package br.com.alura.domain;
 
 public class Pet {
+    private String tipo;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String cor;
+    private Float peso;
+
+    public Pet() {
+    }
+
+    public Pet(String tipo, String nome, String raca, int idade, String cor, Float peso) {
+        this.tipo = tipo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
 }
