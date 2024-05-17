657e76d36185a471b39c779482c1d2844de426f8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-17 09:21:06 -0300

    Curso 01 - Capítulo 04 - Aula 06: Validar responseBody

--- src/main/java/br/com/alura/service/AbrigoService.java
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -26,11 +26,11 @@ public class AbrigoService {
         Abrigo[] abrigos = new ObjectMapper().readValue(responseBody, Abrigo[].class);
         List<Abrigo> abrigoList = Arrays.stream(abrigos).toList();
 
-        System.out.println("Abrigos cadastrados:");
-        for (Abrigo abrigo : abrigoList) {
-            long id = abrigo.getId();
-            String nome = abrigo.getNome();
-            System.out.println(id +" - " +nome);
+        if(abrigoList.isEmpty()){
+            System.out.println("Não há abrigos cadastrados...");
+        }
+        else {
+            mostrarAbrigos(abrigoList);
         }
     }
 
@@ -56,6 +56,15 @@ public class AbrigoService {
         } else if (statusCode == 400 || statusCode == 500) {
             System.out.println("Erro ao cadastrar o abrigo:");
             System.out.println(responseBody);
+        }
+    }
+
+    private void mostrarAbrigos(List<Abrigo> abrigos){
+        System.out.println("Abrigos cadastrados:");
+        for (Abrigo abrigo : abrigos) {
+            long id = abrigo.getId();
+            String nome = abrigo.getNome();
+            System.out.println(id +" - " +nome);
         }
     }
 }
